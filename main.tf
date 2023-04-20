ef32bd1111ac42224fbe4d1726a3b028cef74181
Vineet Sharma <thevman@users.noreply.github.com>
2023-04-19 23:22:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,17 +8,16 @@ terraform {
 
   # Update this block with the location of your terraform state file
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-github-actions-state"
-    storage_account_name = "terraformgithubactions"
-    container_name       = "tfstate"
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstate34923493"
+    container_name       = "ghtfstate"
     key                  = "terraform.tfstate"
-    use_oidc             = true
   }
 }
 
 provider "azurerm" {
   features {}
-  use_oidc = true
+  use_msi = true
 }
 
 # Define any Azure resources to be created here. A simple resource group is shown here as a minimal example.
